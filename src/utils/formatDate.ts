f817dc7c9bbf49d0562a10112e3e1bde093c92d4
m1guer <miguelpintojose00@gmail.com>
2025-05-30 16:26:45 -0300

    wip

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,24 +1,24 @@
-import { InfraError } from '@/services/errors/InfraError'
-import { format as formatFNS } from 'date-fns'
+import { InfraError } from "@/services/errors/InfraError";
+import { format as formatFNS } from "date-fns";
 
+export function formatDate(date?: string, format?: string) {
+	try {
+		console.log(date);
+		if (!date) return "";
 
-export function formatDate(date: string, format?: string){
-  try {
-    if (!date) return ''
-
-    return formatFNS(new Date(date), format || 'dd/MM/yyyy')
-  } catch (error) {
-    throw new InfraError({
-      originalError: error,
-      titleMessage: 'Erro ao formatar data',
-      message: '',
-      metadata: {
-        name: 'function formatDate',
-        date,
-        ...(format && {
-          formato: format,
-        })
-      }
-    })
-  }
+		return formatFNS(new Date(date), format || "dd/MM/yyyy");
+	} catch (error) {
+		throw new InfraError({
+			originalError: error,
+			titleMessage: "Erro ao formatar data",
+			message: "",
+			metadata: {
+				name: "function formatDate",
+				date,
+				...(format && {
+					formato: format,
+				}),
+			},
+		});
+	}
 }
